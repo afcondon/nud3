520bded7eefc1d5ee76c28ffbeccb7955cf177a2
Andrew Condon <andrew.condon@gmail.com>
2023-03-10 14:43:42 +0100

    add examples to main (still no implementation)
    next step to add logging and eventually implementation of the API so that the examples actually do something.
    
    Initially they will just be stubs so that it can run in Node and then we’ll bundle it and use Web.DOM etc to make marks on (virtual) paper in the browser

--- src/Main.purs
+++ src/Main.purs
@@ -5,8 +5,18 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Examples.GUP (generalUpdatePattern)
+import Examples.Matrix (matrix2table)
+import Examples.Miserables (drawForceLayout)
+import Examples.ThreeLittleCircles (threeLittleCircles)
+import Examples.Tree (drawTree)
 
 main :: Effect Unit
 main = do
+  matrix2table
+  threeLittleCircles
+  generalUpdatePattern
+  -- drawTree 
+  -- drawForceLayout
   log "🍝"
 
