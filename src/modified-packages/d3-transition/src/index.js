95d8ef10a9e57a2b428efb4338cb7ed345c90e6a
Andrew Condon <andrew.condon@gmail.com>
2023-03-26 13:41:56 +0200

    remove the modified files again
    realized that they weren’t even being picked up from here instead i’d modified the d3-transitions.js file in `node-modules`

--- src/modified-packages/d3-transition/src/index.js
+++
@@ -1,5 +0,0 @@
-import "./selection/index.js";
-export {default as transition} from "./transition/index.js";
-export {Transition} from "./transition/index.js"; // afc edit
-export {default as active} from "./active.js";
-export {default as interrupt} from "./interrupt.js";
