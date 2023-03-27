3a8cf416eecd53b25b29d715c3f49e72c3d6e169
Andrew Condon <andrew.condon@gmail.com>
2023-03-27 09:06:11 +0200

    removing d3 object refs to scales
    allows visualization to complete - looks like it’s wrong in certain ways but all nodes and links are in there

--- src/Nud3/Scales.js
+++ src/Nud3/Scales.js
@@ -8,17 +8,17 @@ export function d3SchemePairedN_(number) { return d3SchemePaired(number) }
 export function d3SchemeCategory10N_(number) { return d3SchemeCategory10(number) }
 export function d3SchemeSequential10N_(number) { return d3SchemeSequential10(number) }
 
-const d3SchemeCategory10 = d3.scaleOrdinal(d3.schemeCategory10)
+const d3SchemeCategory10 = scaleOrdinal(schemeCategory10)
 export function d3SchemeCategory10S_(string) { return d3SchemeCategory10(string) }
 
-const d3SchemePaired = d3.scaleOrdinal(d3.schemePaired)
+const d3SchemePaired = scaleOrdinal(schemePaired)
 export function d3SchemePairedS_(string) { return d3SchemePaired(string) }
 
-const d3SchemeDiverging10 = d3.scaleDiverging(d3.interpolateBrBG)
+const d3SchemeDiverging10 = scaleDiverging(interpolateBrBG)
     .domain([0, 250, 500]); // TODO this should be determined by number of nodes in sim
 
-const d3SchemeSequential10 = d3.scaleSequential()
-    .interpolator(d3.interpolateYlOrRd)
+const d3SchemeSequential10 = scaleSequential()
+    .interpolator(interpolateYlOrRd)
     .domain([0, 5, 10]); // TODO this should be determined by number of nodes in sim
 
 
